0e4ad65ba2850c02d6e3c80d6980f37c83f564e1
Anton Pashkovskyi <anton@globalbit.co.il>
2024-09-26 13:57:44 +0200

    packages: add sendMoneyData model

--- packages/deplan_core/lib/deplan_core.dart
+++ packages/deplan_core/lib/deplan_core.dart
@@ -1,7 +1,3 @@
 library deplan_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:deplan_core/models/deplan_models.dart';
---
+++ packages/deplan_core/lib/models/deplan_models.dart
@@ -0,0 +1,1 @@
+export 'package:deplan_core/models/send_money_data_model.dart';
