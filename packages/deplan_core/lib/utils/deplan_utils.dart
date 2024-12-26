f4ae87a2821e8791ff6c0265ef2cf806b49e7e87
Anton Pashkovskyi <antonpash.dev@gmail.com>
2024-12-26 18:33:10 +0100

    core add validation utils

--- packages/deplan_core/lib/utils/deplan_utils.dart
+++ packages/deplan_core/lib/utils/deplan_utils.dart
@@ -1,3 +1,4 @@
 export 'package:deplan_core/utils/html_shim.dart';
 export 'package:deplan_core/utils/web_plugins_shim.dart';
 export 'package:deplan_core/utils/numbers.dart';
+export 'package:deplan_core/utils/validation.dart';
